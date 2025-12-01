a57b8682477d7e207879a8b9e72904c2fd5d4493
TriangulumAustrale <93359936+TriangulumAustrale@users.noreply.github.com>
2025-11-30 19:53:36 -0600

    Update triple_DES.py
    added comments

--- algorithms/triple_DES.py
+++ algorithms/triple_DES.py
@@ -7,23 +7,23 @@ passwordSalt = b'\\`\xd6\xdaB\x03\xdd\xd4z\xb6p\xe8O\xf0\xa8\xc0'
 iv = 11357323082506326930  
 
 
-def _make_counter(iv_int):
+def _make_counter(iv_int): #counter to track the file
     while True:
-        yield iv_int.to_bytes(16, "big")
-        iv_int = (iv_int + 1) & ((1 << 128) - 1)
+        yield iv_int.to_bytes(8, "big")
+        iv_int = (iv_int + 1) & ((1 << 64) - 1)
 
 
-def encrypt(raw, password):
+def encrypt(raw, password): 
     key = pbkdf2.PBKDF2(password, passwordSalt).read(24)
-    key = DES3.adjust_key_parity(key)
+    key = DES3.adjust_key_parity(key) #adjusts the pairty of the key to ensure it follows 3des rules
 
-    cipher = DES3.new(key, DES3.MODE_ECB)
+    cipher = DES3.new(key, DES3.MODE_ECB) #creates an object to encrypt the data
 
-    counter = _make_counter(iv)
+    counter = _make_counter(iv)  #creates a generator for the key
     raw_bytes = raw.encode("utf-8")
     encrypted = b""
 
-    for i in range(0, len(raw_bytes), 8):
+    for i in range(0, len(raw_bytes), 8): #encrypts the data
         block = raw_bytes[i:i+8]
         keystream = cipher.encrypt(next(counter)[:8])
         encrypted += bytes(a ^ b for a, b in zip(block, keystream))
@@ -32,17 +32,17 @@ def encrypt(raw, password):
 
 
 def decrypt(cipherText, password):
-    cipher_bytes = binascii.unhexlify(cipherText.encode("utf-8"))
+    cipher_bytes = binascii.unhexlify(cipherText.encode("utf-8")) #converts to hex
 
-    key = pbkdf2.PBKDF2(password, passwordSalt).read(24)
+    key = pbkdf2.PBKDF2(password, passwordSalt).read(24) #generates the key
     key = DES3.adjust_key_parity(key)
 
-    cipher = DES3.new(key, DES3.MODE_ECB)
+    cipher = DES3.new(key, DES3.MODE_ECB) 
 
     counter = _make_counter(iv)
     decrypted = b""
 
-    for i in range(0, len(cipher_bytes), 8):
+    for i in range(0, len(cipher_bytes), 8):  #loop through and decode
         block = cipher_bytes[i:i+8]
         keystream = cipher.encrypt(next(counter)[:8])
         decrypted += bytes(a ^ b for a, b in zip(block, keystream))
